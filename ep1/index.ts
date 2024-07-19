94b46e13c80bb6c9ed1a6a423600025480de6a50
pimchanok04 <pimchanok29042004@gmail.com>
2024-07-19 16:13:33 +0700

    2

--- ep1/index.ts
+++ ep1/index.ts
@@ -7,3 +7,10 @@ var want_to_go_home : boolean = true
 console.log(firstname)
 console.log(y)
 console.log(want_to_go_home)
+
+
+const a : number = 4
+const b : number = 6
+console.log(a===b)
+console.log(a>b)
+console.log(a<=b)
