33f39c4f57e0c82db10ba420b12bf04e29b8648b
pimchanok04 <pimchanok29042004@gmail.com>
2024-08-02 10:48:36 +0700

    ooo

---
+++ ep.3/homework.ts
@@ -0,0 +1,18 @@
+function shape(height: number , weight: number) {
+    let shape = weight /  (height * height) ;
+
+    if (shape < 18.5) {
+        return 'ตํ่ากว่าเกณฑ์' ;
+    } else if (shape >= 18.5 && shape < 22.9) {
+        return 'ปกติ';
+    } else if (shape >= 23 && shape < 24.9) {
+        return 'เริ่มอ้วน';
+    } else  if (shape >= 25 && shape < 29.9) {
+        return 'อ้วน';
+    } else if (shape > 30) {
+        return 'Very fat'
+    } else {
+        return 'Error'
+    }
+}
+console.log(bmi(1.58,42)
