4490322c5d28e4ca5a394a00ec43621051d47120
pimchanok04 <pimchanok29042004@gmail.com>
2024-08-02 14:01:19 +0700

    eiei

---
+++ ep 4/index.ts
@@ -0,0 +1,18 @@
+function sayHi(dayOfWeek: number) {
+    if (dayOfWeek === 1) {
+        console.log('สวัสดีวันอาทิตย์')
+    } else if (dayOfWeek === 2) {
+        console.log('สวัสดีวันจันทร์')
+    } else if (dayOfWeek === 3) {
+        console.log('สวัสดีวันอังคาร')
+    } else if (dayOfWeek === 4) {
+        console.log('สวัสดีวันพุธ')
+    } else if (dayOfWeek === 5) {
+        console.log('สวัสดีวันพฤหัสบดี')
+    } else if (dayOfWeek === 6) {
+        console.log('สวัสดีวันศุกร์')
+    } else if (dayOfWeek === 7) {
+        console.log('สวัสดีวันเสาร์')
+    }
+}
+sayHi(7)
