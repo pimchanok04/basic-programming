f32d3108514af95cb6842985b041aa51408d00ea
pimchanok04 <pimchanok29042004@gmail.com>
2024-08-23 14:22:55 +0700

    lol

---
+++ ep 5/index.ts
@@ -0,0 +1,30 @@
+// let 5sum: number = 0
+
+// for (let i = 1; 1 <= 31; 1+1) {
+//     sum = sum + 1 
+// }
+
+// console.log('Total: ', sum)
+
+// let sum: number = 0
+// let i: number = 1
+
+// while (i <= 31) {
+//       sum = sum + 1
+//       i++
+// }
+
+// console.log('Total; ', sum)
+
+// let sum: numbert = 0
+// let i: number = 1
+
+// do {
+// sum = sum + 1
+// i++
+// } while (i <= 31)
+
+// console.log('Total: ', sum)
+
+
+
