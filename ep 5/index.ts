8e8f34bd4cd00d0af8f7bfa6aa623b46c9000f47
pimchanok04 <pimchanok29042004@gmail.com>
2024-08-23 14:46:18 +0700

    yaya

--- ep 5/index.ts
+++ ep 5/index.ts
@@ -37,3 +37,21 @@ while (true) {
         break
     }
 }
+
+
+
+for (let i = 1; i <= 20; i++) {
+    console.log('ฉันอายุ ' + i + 'ปี')
+}
+
+let i: number = 1
+while (i <= 20) {
+    console.log('ฉํนอายุ ' + i + 'ปี')
+    i++
+}
+
+let i: number = 1
+do {
+    console.log('ฉัันอายุ ' + i + 'ปี')
+    i++
+} while (i <= 20)
