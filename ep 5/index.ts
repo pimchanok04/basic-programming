208a2918118a0f5558066aa991a15e6b909aa62c
pimchanok04 <pimchanok29042004@gmail.com>
2024-09-05 20:23:04 +0700

    nonooooooo

---
+++ ep 5/index.ts
@@ -0,0 +1,11 @@
+const str : string = "นายณัฐกานต์ เด็กติดเกม Roblox aka ทิวคุง"
+
+console.log(str.split(" "))
+
+const str : string = "ยายเเล่ม เมื่อตอนสาว ๆ ผิวขา ตาคม ผมยาว"
+
+console.log(str.replace("ยายเเล่ม", "โรนัลโด้"))
+
+const str : string = "นายทดสอบ สุดหล่อ"
+
+console.log(str.replaceAll("นาย", " "))
