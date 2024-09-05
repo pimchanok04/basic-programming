d6c55a49d1ce36165e7a09e702776f67785c95e3
pimchanok04 <pimchanok29042004@gmail.com>
2024-09-05 20:12:42 +0700

    narakkk

---
+++ ep 7/homework.ts
@@ -0,0 +1,26 @@
+const personalinformation = {
+    name: 'mylittlepony',
+    cerrentAddressNumber: '99',
+    villageNo: '55',
+    street: 'babyyah',
+    subDistrict: 'rarity',
+    disrict: 'twilight',
+    proVince: 'ponyville',
+    zipCode: '57902',
+    telePhone: '0777777777',
+    mobilePhone: '02222222222',
+    email: 'ponyyyyahhh@gmail.com'
+}
+console.log(personalinformation)
+// ประวัติส่วนตัว
+// ชื่อ: มายลิตเติ้ลโพนี่
+// ที่อยู๋ปัจจจุบันเลขที่: 99
+// หมู๋ที่: 55
+// ถนน: เบบี้ย๊าห์
+// ตำบล​​​​: แรริตี้
+// อำเภอ: ทไวไลท์
+// จังหวัด: โพนี่วิลล์
+//  รหัสไปรษณีย์: 57902
+// โทรศัพท์: 0777777777
+// มือถือ: 0222222222
+// อีเมล์: ponyyyyahhh@gmail.com
