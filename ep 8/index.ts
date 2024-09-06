d903146dc7d9f851d7ba0f2fa68551e3309d77d3
pimchanok04 <pimchanok29042004@gmail.com>
2024-09-06 13:42:29 +0700

    kieee

---
+++ ep 8/index.ts
@@ -0,0 +1,24 @@
+const aboutMe = {
+    firstName: 'Elle',
+    lastName:'Super',
+    age: 14,
+    mail: 'mmm@f*@.comma',
+    phone: '07***87887',
+    idCard: 'xx345xxxx',
+    fan: {
+        fistName: 'Jennie',
+        lastName: 'Kim',
+        age: 24,
+        animal: {
+            animalType: 'Dog',
+            color: 'gold',
+        }
+    },
+    myBro: {
+        fistname: 'ink',
+        lasName: 'inkkie',
+        AKA: 'nongink',
+    }
+}
+
+console.log(aboutMe.myBro.AKA)
