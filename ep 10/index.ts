22409f246819695409af91eb8dcf86a1d58bc8f1
pimchanok04 <pimchanok29042004@gmail.com>
2024-09-20 16:37:21 +0700

    popopopopo

--- ep 10/index.ts
+++ ep 10/index.ts
@@ -37,3 +37,74 @@ function register(email: string, password: string) {
 }
 
 register('pony@gmail.com', '25559999777')
+
+
+
+const database = [
+    {
+        email: 'user@gmail.com',
+        password: 'loveYou3000'
+    },
+    {
+        email: 'pony@gmail.com',
+        password: '25559999777'
+    }
+]
+
+function login(email: string, password: string) {
+    const user = database.filter(function(element, index) {
+        return element.email === email
+    })
+    if (user.length > 0) {
+        if (user[0]. password === password) {
+        alert('เข้าสู่ระบบสำเร็จ ยินดีต้อนรับ')
+    } else {
+        alert('รหัสผ่า่นไม่ถูกต้อง โปรดตรวจสอบ')
+    }  
+    } else {
+        alert('ไม่พบผู้ใช้งานในระบบ')
+    }
+}
+
+login('pony@gmail.com', '25559999777')
+
+
+
+
+// กฎการเข้ารหัส
+// เปลี่ยน u เป็น +
+//เปลี่ยน 9 เป็น *
+// เปลี่ยน 0 เป็น ^
+const database = [
+    {
+        email: 'user@gmail.com',
+        password: 'loveYo+3^^^'
+    },
+    {
+        email: 'pony@gmail.com',
+        password: '2555****777'
+    }
+]
+
+function decryptPassword(password: string) {
+    return password.replaceAll('+','u').replaceAll('*','9').replaceAll('^','0')
+}
+
+function login(email: string, password: string) {
+    const user = database.filter(function(element, index) {
+        return element.email === email
+    })
+    if (user.length > 0) {
+        const realPassword = decryptPassword(user[0].password)
+
+        if (user[0]. password === password) {
+        alert('เข้าสู่ระบบสำเร็จ ยินดีต้อนรับ')
+    } else {
+        alert('รหัสผ่า่นไม่ถูกต้อง โปรดตรวจสอบ')
+    }  
+    } else {
+        alert('ไม่พบผู้ใช้งานในระบบ')
+    }
+}
+
+login('pony@gmail.com', '2555****777')
