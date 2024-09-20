2489ac0c17fe4a60be1803407bbc5b848c0d2696
pimchanok04 <pimchanok29042004@gmail.com>
2024-09-20 15:11:59 +0700

    rtrtr

--- ep 10/index.ts
+++ ep 10/index.ts
@@ -7,3 +7,33 @@ setTimeout(() => {
 })
 
 console.log("5")
+
+
+// zane.com -> No
+// tan@ -> No
+// safe@utk.ac.th -> yes
+// poppy2gmail.com -> Yes
+const database = [
+    {
+        email: 'user@gmail.com',
+        password: 'loveYoo3000'
+    }
+]
+function register(email: string, password: string) {
+    if (email.includes('@') && email.lastIndexOf('@') !== email.length - 1) {
+         if (password.length > 8 && password.length < 16) {
+             const userObject = {
+                email: email,
+                password: password
+             }
+             database.push(userObject)
+             alert('สมัครสมาชิกแล้ว ยยยินดีต้อนรับคุณ' + email)
+         }else{
+            alert('รหัสผ่านต้องมากกว่า 8 ตัว และไม่เกิน 16')
+         }
+    }else {
+      alert('Emailไม่ถูกต้อง')
+    }
+}
+
+register('pony@gmail.com', '25559999777')
