154fd8be73f6d5f939fb512dc6a6ba45b782118c
pimchanok04 <pimchanok29042004@gmail.com>
2024-09-27 14:08:20 +0700

    ooo

---
+++ ep 11/index.ts
@@ -0,0 +1,28 @@
+const database = [
+    {
+        email: 'max@utk.ac.th',
+        password: 'maxi@utk1234'
+    }
+]
+
+function login(email: string, password: string) {
+    const user = database.filter(function (element, index) {
+        return element.email === email
+    }) // b [{
+    // email: 'max@utk.ac.th
+    // password: 'max@utk1234
+    // }] || []
+
+    if (user.length > 0) {
+        if (user[0].password === password) {
+            console.log('เข้าสู๋ระบบแล้ว')
+        } else {
+            console.log('รหัสผ่านไม่ถูกต้อง')
+        }
+    } else {
+        console.log('ไม่มีผู้ใช้งานในระบบ')
+    }
+}
+
+login('max@utk.ac.th','maxi@utk1234')
+
